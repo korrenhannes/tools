d1702e629bc61e061db250bcf1153fcff0a44185
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2024-01-14 16:58:22 +0200

    a

--- chatbot.py
+++ chatbot.py
@@ -107,7 +107,8 @@ class InstagramBot:
         try:
             wait = WebDriverWait(self.driver, 10)
             # Updated XPath to match your provided HTML element
-            direct_message_icon = wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@aria-label='Direct messaging - 1 new notification link']")))
+            direct_message_icon = wait.until(EC.element_to_be_clickable(
+                (By.XPATH, "//a[contains(@aria-label, 'Direct messaging - ') and contains(@aria-label, 'new notification')]")))
             direct_message_icon.click()
             time.sleep(5)
             return True
