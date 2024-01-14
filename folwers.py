9d6de8119a0879f50b681aa4af17dcbd765540ad
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2024-01-15 01:11:27 +0200

    a

--- folwers.py
+++ folwers.py
@@ -237,8 +237,8 @@ class InstagramBot:
         self.run_shell_command("pkill caffeinate")
 
 if __name__ == "__main__":
-    USERNAME = 'doronytoto1232345'
-    PASSWORD = '2HLqF,B*vk!,h;x'
+    USERNAME = 'tomclipper1231232345'
+    PASSWORD = 'kokojaja123'
     TARGET_USERS = ['kindweirdwild', 'wordofmachine']
 
     bot = InstagramBot(USERNAME, PASSWORD, TARGET_USERS)
