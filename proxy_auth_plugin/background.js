eea6e3b9268dbee6b34f5969190141831b121f56
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2024-01-15 20:03:47 +0200

    a

--- proxy_auth_plugin/background.js
+++ proxy_auth_plugin/background.js
@@ -1,28 +1,28 @@
 var config = {
-    mode: "fixed_servers",
-    rules: {
-      singleProxy: {
-        scheme: "http",
-        host: "YOUR_PROXY_HOST",
-        port: parseInt(YOUR_PROXY_PORT)
-      },
-      bypassList: ["localhost"]
-    }
-  };
+  mode: "fixed_servers",
+  rules: {
+    singleProxy: {
+      scheme: "http",
+      host: "gate.smartproxy.com", // Your proxy host
+      port: parseInt(10001) // Your proxy port
+    },
+    bypassList: ["localhost"]
+  }
+};
 
 chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
 
 function callbackFn(details) {
-    return {
-        authCredentials: {
-            username: "YOUR_PROXY_USERNAME",
-            password: "YOUR_PROXY_PASSWORD"
-        }
-    };
+  return {
+      authCredentials: {
+          username: "sp9zw4gx22", // Your proxy username
+          password: "kXeSr49iPa5oxhLw3z" // Your proxy password
+      }
+  };
 }
 
 chrome.webRequest.onAuthRequired.addListener(
-        callbackFn,
-        {urls: ["<all_urls>"]},
-        ['blocking']
+      callbackFn,
+      {urls: ["<all_urls>"]},
+      ['blocking']
 );
